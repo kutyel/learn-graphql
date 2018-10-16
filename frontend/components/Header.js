ced10a4a88b6a6f9ab884ffedb3dfc226e22b807
kutyel <flavio.corparios@ext.soprasteria.com>
2018-10-16 16:05:18 +0200

    Start some styling 💅

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,14 +1,59 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+
 import Nav from './Nav'
 
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+  z-index: 2;
+  a {
+    padding: 0.5rem 1rem;
+    background: ${({ theme }) => theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`
+
+const Bar = styled.header`
+  border-bottom: 10px solid ${({ theme }) => theme.black};
+  display: grid;
+  grid-template-columns: auto 1fr;
+  justify-content: space-between;
+  align-items: stretch;
+  @media (max-width: 1300px) {
+    grid-template-columns: 1fr;
+    justify-content: center;
+  }
+}
+`
+
+const SubBar = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  border-bottom: 1px solid ${({ theme }) => theme.lightgrey};
+`
+
 export default () => (
-  <div>
-    <div className="bar">
-      <a href="/">Sick Fits</a>
+  <header>
+    <Bar>
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
-    </div>
-    <div className="sub-bar">
+    </Bar>
+    <SubBar>
       <p>Search</p>
-    </div>
+    </SubBar>
     <div>Cart</div>
-  </div>
+  </header>
 )
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,35 @@
-import Header from './Header'
+import styled, { ThemeProvider } from 'styled-components'
+
 import Meta from './Meta'
+import Header from './Header'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const Page = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.color};
+`
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 3rem;
+`
 
 export default ({ children }) => (
-  <div>
-    <Meta />
-    <Header />
-    {children}
-  </div>
+  <ThemeProvider theme={theme}>
+    <Page>
+      <Meta />
+      <Header />
+      <Inner>{children}</Inner>
+    </Page>
+  </ThemeProvider>
 )
