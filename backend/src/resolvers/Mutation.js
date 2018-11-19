1b5ae59265525a3742b2c7a6fa70bc8fc95a0d24
kutyel <flavio.corparios@ext.soprasteria.com>
2018-11-19 14:52:10 +0100

    Finish with server-side GraphQL! 🚀

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutations = {}
+const Mutations = {
+  createItem(_, args, { db }, info) {
+    // TODO: check if logged in
+    return db.mutation.createItem(
+      {
+        data: { ...args },
+      },
+      info
+    )
+  },
+}
 
 module.exports = Mutations
