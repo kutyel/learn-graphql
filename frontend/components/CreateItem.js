166258eb3b29966f97c2936a78520877d71e0bbd
kutyel <flavio.corparios@ext.soprasteria.com>
2018-12-04 14:52:34 +0100

    Upload Images! 📸

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -36,6 +36,21 @@ export default class extends Component {
   }
   handleChange = ({ target: { type, name, value } }) =>
     this.setState({ [name]: type === 'number' ? +value : value })
+  uploadFile = async e => {
+    const { files } = e.target
+    const body = new FormData()
+    body.append('file', files[0])
+    body.append('upload_preset', 'sickfits')
+    const res = await fetch('https://api.cloudinary.com/v1_1/kutyel/image/upload', {
+      method: 'POST',
+      body,
+    })
+    const {
+      secure_url: image,
+      eager: [{ secure_url: largeImage }],
+    } = await res.json()
+    this.setState({ image, largeImage })
+  }
   render = () => (
     <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
       {(createItem, { loading, error }) => (
@@ -55,6 +70,18 @@ export default class extends Component {
         >
           <Error error={error} />
           <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+              Image
+              <input
+                type="file"
+                name="file"
+                id="file"
+                placeholder="Upload an Image"
+                required
+                onChange={this.uploadFile}
+              />
+              {this.state.image && <img src={this.state.image} alt="Upload Preview" />}
+            </label>
             <label htmlFor="title">
               Title
               <input
